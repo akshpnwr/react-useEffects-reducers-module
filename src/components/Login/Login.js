2b2fff5c9ae01092733cca12e1614b8aaea828e3
akshpnwr <akashpanwar9976@gmail.com>
2021-05-23 13:40:36 +0530

    setting timeout

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('checking validity');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('cleanup');
+      clearTimeout(identifier);
+    };
     // if (emailIsValid && passwordIsValid) setFormIsValid(true);
     // else setFormIsValid(false);
   }, [enteredEmail, enteredPassword]);
